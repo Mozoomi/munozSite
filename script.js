6c9cab9a22b4994925647d7e6fd38d7b3d99878c
Mozoomi <moza12tv@gmail.com>
2024-11-10 22:32:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", function() {
+    const scrollDistance = 250;
+    const nav = document.querySelector(".top-nav");
+    const secondNav = document.querySelector(".second-nav");
+    const secondScrollDistance = 500;
+
+    if (window.scrollY > scrollDistance) {
+        nav.classList.add("hide-nav");
+    } else {
+        nav.classList.remove("hide-nav");
+    }
+
+    if (window.scrollY > secondScrollDistance) {
+        secondNav.classList.add("show");
+    } else {
+        secondNav.classList.remove("show");
+    }
+});
